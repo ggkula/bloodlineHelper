6c848c531dcfbfb291efc4c4aed62bc85398d8bb
ggkula <s335631892@icloud.com>
2018-01-24 17:58:17 +0800

    add some data

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -5,6 +5,7 @@
 <script>
   import header from 'header';
   import line from 'line';
+  import rhythm_plate from '../global/plate'
 
   export default {
     name: "index",
@@ -13,7 +14,8 @@
         title:'',                 //标题
         rhythmVersion:[],         //节奏榜版本
         archerVersion:[],         //射手榜版本
-        listType:''               //榜的类型
+        listType:'r',             //榜的类型，r=节奏榜，a=射手榜，默认为r
+        plate:rhythm_plate[0]     //榜的板式
       }
     },
     components:{
