5f803043d073efb3513191fb7043308b505cf059
ggkula <s335631892@icloud.com>
2018-01-24 16:09:27 +0800

    no message

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -3,9 +3,23 @@
 </template>
 
 <script>
-    export default {
-        name: "index"
+  import header from 'header';
+  import line from 'line';
+
+  export default {
+    name: "index",
+    data(){
+      return{
+        title:'',                 //标题
+        rhythmVersion:[],         //节奏榜版本
+        archerVersion:[],         //射手榜版本
+        listType:''               //榜的类型
+      }
+    },
+    components:{
+      header,line,
     }
+  }
 </script>
 
 <style scoped>
