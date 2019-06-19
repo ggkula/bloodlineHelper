d37e0210b0000b1210fb47c44b3766de888a4709
ggkula <s335631892@icloud.com>
2019-06-19 12:50:49 +0800

    tinied jpgs & add 2 new cards' data

--- src/data/data.ts
+++ src/data/data.ts
@@ -5,8 +5,10 @@ import {EachData} from '../app/modal/dataModal'
 import id0 from './resolve/dark/奥'
 import id1 from './resolve/dark/巡'
 import id2 from './resolve/light/露'
+import id3 from './resolve/fire/萝莉'
+import id4 from './resolve/light/德'
 
-const dataList: EachData[] = [id0, id1, id2];
+const dataList: EachData[] = [id0, id1, id2, id3, id4];
 
 export default dataList.sort((a, b) => {
     return a.id - b.id
---
+++ src/data/resolve/light/德.ts
@@ -0,0 +1,36 @@
+import {EachData} from '../../../app/modal/dataModal'
+
+const theData: EachData = {
+    name: '德丽莎',
+    id: 4,
+    attr: {
+        key: 'light',
+        value: '光'
+    },
+    job: '战士',
+    transfer: ['学院长', '战神', '饮命者'],
+    camp: '异族',
+    avatar: '/assets/avatar/light/德丽莎.png',
+    card: [{
+        type: '20血量学园长德丽莎',
+        src: '/assets/card/light/德.jpg'
+    }, {
+        type: '20攻击学园长德丽莎',
+        src: '/assets/card/light/德g.jpg'
+    }, {
+        type: '20攻击饮命者莉萝艾',
+        src: '/assets/card/light/德8.jpg'
+    }],
+    skill: '/assets/skill/dark/巡.jpg',
+    resolves: [
+        {
+            auth: '',
+            date: '',
+            paragraph: [
+                ''
+            ]
+        }
+    ]
+};
+
+export default theData
