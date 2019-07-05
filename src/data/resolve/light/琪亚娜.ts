41598e51ed5d5af4fd3b3fdd72e0ab9fbd9a6ee0
ggkula <s335631892@icloud.com>
2019-07-05 20:05:30 +0800

    update

--- src/data/resolve/light/琪亚娜.ts
+++ src/data/resolve/light/琪亚娜.ts
@@ -16,7 +16,7 @@ const theData: EachData = {
         src: '/assets/card/light/琪亚娜.jpg'
     }],
     skill: [{
-        skinName: '莉娅丝',
+        skinName: '琪亚娜',
         leader: '队伍中琪亚娜、八重樱、布洛妮娅、雷电芽衣、德丽莎攻击和血量增加20%',
         talent: '装备升级的速度变快,与八重樱、布洛妮娅、雷电芽衣、德丽莎中任意一人同时在场时，自身的攻击增加10%',
         utl: '琪亚娜对敌方全体造成中量光属性伤害',
