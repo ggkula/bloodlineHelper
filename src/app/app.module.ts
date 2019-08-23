d8bdfb1d9351f28017f466a69885858e3eeee0cc
ggkula <s335631892@icloud.com>
2019-08-23 16:14:47 +0800

    v0.9.7 ng update & sw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,8 @@ import { BorderDirective } from './directive/border.directive';
 import { SkillBoardComponent } from './component/skill-board/skill-board.component';
 import { UpdateInfoListComponent } from './page/update-info-list/update-info-list.component';
 import { UpdateInfoDetailComponent } from './page/update-info-detail/update-info-detail.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 registerLocaleData(zh);
 
@@ -54,7 +56,8 @@ registerLocaleData(zh);
     NgZorroAntdModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
